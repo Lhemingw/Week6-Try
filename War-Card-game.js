ed41f126ea62200712d699e25db39191603fc801
Lhemingw <lhemingw@gmail.com>
2023-07-20 16:18:33 -0600

    Working game

--- War-Card-game.js
+++ War-Card-game.js
@@ -6,7 +6,7 @@ class Deck {
     constructor(){
         const suits = ["♠", "♣", "♥", "♦"]
         const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-        let deck =[];
+        this.deck =[];
 
      for (let i = 0; i < suits.length; i++) {
         for (let x = 0; x < ranks.length; x++) {            
@@ -16,77 +16,104 @@ class Deck {
                 value: x,
                
             }
-             deck.push(card);
-            }
-        
+             this.deck.push(card);
+            }  
         }
-
-         //shuffle the deck below this and before the return.
-    //      let i =deck.length;
-    //            for (let i = deck.length - 1; i > 0; i--) {
-    //                const j = Math.floor(Math.random() * (i + 1));
-    //               [deck[i], deck[j]] = [deck[j], deck[i]];
-    //            }
-    //        }
-             
-    //  }; 
-    // //  let i = deck.length;
-    //   while (--i > 0) {
-    //       let randIndex = Math.floor(Math.random() -0.5);
-    //       [deck[randIndex], deck[i]] = [deck[i], deck[randIndex]];
-    //   }
-          console.log(deck);
-  
-         let newDeck = new Deck();
-         console.log(newDeck);
-          const split1 = newDeck.slice(0-26);
-          const split2 = newDeck.slice(27-52);
-          console.log(split1);
-          console.log(split2);
-        
-        
+          
         //deck.sort(() => Math.random() - 0.5); Michael's shuffle
         //fisher yates shuffle method
-           
-
-    // }
-
-        
-    
          }
-         shuffle(){
-            const shuffle = () => {
-                for (let i = this.deck.length - 1; i > 0; i--) { 
-                  const j = Math.floor(Math.random() * (i + 1)); 
-                  [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]]; 
-                } 
-                return this.deck; 
-              }; 
-                
-              // Usage 
-              const deck = []; 
-              const shuffledDeck = shuffle(deck); 
-              
-              console.log(deck);  
+         shuffle(){ 
+          //console.log(this.deck);     
+            for (let i = this.deck.length - 1; i > 0; i--) {
+              let j = Math.floor(Math.random() * (i + 1));
+              [this.deck[j], this.deck[i]] = [this.deck[i], this.deck[j]];
             
-         }
-         split(){
-            let newDeck = new Deck();
-         console.log(newDeck);
-          const split1 = newDeck.slice(0-26);
-          const split2 = newDeck.slice(27-52);
-          console.log(split1);
-          console.log(split2);
-        
-        
-         }
-     }
-     let newDeck = new Deck(); 
-     newDeck.shuffle();
-     
-class players {
+        }
+              
+        }
+        deal() {
+          return this.deck.pop();
 
-     }
+        }
+
+} //deck
+      
+class Players {
+    constructor(name) {
+      this.name = name;
+      this.score = 0;
+      this.hand = [];
+    }
+  
+    getName() {
+      return this.name;
+    }
+  
+    getScore() {
+      return this.score;
+    }
+  
+    getHand() {
+      return this.hand;
+    }
+  
+    incrementScore() {
+      this.score++;
+    }
+  
+    receiveCard(card) {
+      this.hand.push(card);
+    }
+  } //end for class players
 class Game{
-    
+    constructor(){
+      this.card = new Deck();
+      //card.split();
+      this.player1 = new Players();
+      this.player2 = new Players();
+     
+    }
+    giveCards(){
+      while (this.card.deck.length > 0) {
+        this.player1.receiveCard(this.card.deal());
+        this.player2.receiveCard(this.card.deal());
+      }                 
+    }
+}//end for class game
+ const game = new Game();
+ game.card.shuffle();
+ game.giveCards();
+ for (let i = 0; i < game.player1.hand.length; i++) {
+  console.log("player1 hand");
+  console.log(game.player1.hand[i]);
+  console.log("player2 hand");
+  console.log(game.player2.hand[i]);
+  if (game.player1.hand[i].value > game.player2.hand[i].value) {
+    console.log("player 1 hand is higher");
+    game.player1.incrementScore();
+  }
+  if (game.player1.hand[i].value < game.player2.hand[i].value) {
+    console.log("player 2 hand is higher");
+    game.player2.incrementScore();
+  }
+ if(game.player1.hand[i].value == game.player2.hand[i].value) {
+  console.log("tie hand");
+ }
 }
+console.log("player1 ", game.player1.getScore());
+console.log("player2 ", game.player2.getScore());
+//console.log("tie hand");
+
+//hows score is higher 3 if statement game.player1.getscore, and etc for player2 and ==
+if (game.player1.getScore() > game.player2.getScore()) {
+  console.log("player 1 won");
+ }
+ if (game.player1.getScore() < game.player2.getScore()) {
+   console.log("player 2 won");
+   game.player2.getScore(); //instead of incrementScore
+ }
+ if(game.player1.getScore() == game.player2.getScore()) {
+  console.log("Tie Game");
+ }
+
