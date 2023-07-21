dbf31ef93e597733d5adb7e311527416dbf83010
Lhemingw <lhemingw@gmail.com>
2023-07-21 13:54:59 -0600

    Final push w/More Remarks

--- War-Card-game.js
+++ War-Card-game.js
@@ -1,5 +1,10 @@
 
-
+// Deal 26 Cards to each Player from a Deck of 52 cards.
+// Iterate through the turns where each Player plays a Card.
+// The Player who played the higher card is awarded a point.
+//     Ties result in zero points for both Players.
+// After all cards have been played, display the score and declare the winner.
+// Write a Unit Test using Mocha and Chai for at least one of the functions you write.
 
 //const deck = new Array(suits, ranks, and deck array to hold it, the this. is used to refer to an object
 //this. is used to access other properties and methods from the saem object.
@@ -126,6 +131,6 @@ if (game.player1.getScore() > game.player2.getScore()) {
  }
  //Nothing is done with  a tie.
  if(game.player1.getScore() == game.player2.getScore()) {
-  console.log("Tie Game");
+  console.log("Tie Game", "End Game-is a good movie");
  }
 
